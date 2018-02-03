05ae436fa8d43a8d5bef9058f20b9ee5dc6f5766
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-03 12:48:42 +0100

    Added validations for article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,8 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article has been created"
       redirect_to articles_path
     else
-      render 'new'
+      flash[:danger] = "Article has not been created!"
+      render :new
     end
   end
 
