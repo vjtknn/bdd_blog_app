271a0e20089c7ab0f0a1696e50dc09632a92016b
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-03 21:09:49 +0100

    Implement edit article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -20,13 +20,29 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = 'Article has been updated'
+      redirect_to article_path(@article)
+    else
+      flash.now[:danger] = 'Article has not been updated'
+      render :edit
+    end
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
   protected
---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature 'Editing an article' do
+  before do
+    @article = Article.create(title: "First article", body: "This is a first article")
+  end
+
+  scenario 'A user updates an article' do
+    visit '/'
+
+    click_link @article.title
+    click_link 'Edit Article'
+
+    fill_in 'Title',with: 'Updated title of first article'
+    fill_in 'Body',with: 'Updated body of first article'
+    click_button "Update the article"
+
+    expect(page).to have_content('Article has been updated')
+    expect(page.current_path).to eq(article_path(@article))
+    expect(page).to have_content('Updated title of first article')
+    expect(page).to have_content('Updated body of first article')
+    end
+
+    scenario 'A user fails to update an article' do
+      visit '/'
+
+      click_link @article.title
+      click_link 'Edit Article'
+
+      fill_in 'Title',with: ''
+      fill_in 'Body',with: 'Updated body of first article'
+      click_button "Update the article"
+
+      expect(page).to have_content('Article has not been updated')
+      expect(page.current_path).to eq(article_path(@article))
+      end
+
+end
