0142e2cf13673acd7ed37cdb3ab951a91097da51
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-03 20:26:32 +0100

    Handle article not found error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,4 +28,11 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  protected
+  def resource_not_found
+    message = "The article you are looking for could not be found"
+    flash[:alert] = message
+    redirect_to root_path
+  end
 end
