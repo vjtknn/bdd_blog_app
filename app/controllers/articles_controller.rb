e045661a409424a91181d2191cebe104dbf96567
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-04 12:31:37 +0100

    Sign in green test and styling for signin page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  #before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @articles = Article.all
   end
